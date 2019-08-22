dbc3ee9d9586dea78def510309af1a997e8475fc
studentofcoding <yonathanevan@gmail.com>
2019-08-22 14:46:26 +0700

    (Init) Deep Space Booking System
    Integrated Booking System via :
    - Google Form -> Booking Submit
    - Google Spreadsheet -> Booking List
    - Google Calendar -> Booked Event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+          {/*This is the function to Route to Page (via switch)*/}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/hook" component={Formregisterhook} /> */}
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
